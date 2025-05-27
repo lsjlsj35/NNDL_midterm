943b85b38e4b95fe042c437ef0cdf2a81cca7cf4
lsjlsj35 <103647987+lsjlsj35@users.noreply.github.com>
2025-05-28 06:56:58 +0800

    1

---
+++ mmdetection/tools/visualize_infer_sparse.py
@@ -0,0 +1,122 @@
+from mmengine.config import Config
+from mmengine.runner import Runner
+from mmengine.runner import load_checkpoint
+from mmdet.apis import init_detector, inference_detector
+from mmdet.utils import register_all_modules
+import mmcv
+import os
+from mmdet.visualization.local_visualizer import DetLocalVisualizer
+
+import os
+import cv2
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from mmdet.apis import inference_detector
+from matplotlib.patches import Rectangle
+
+def visualize_prediction_with_masks(model, image_path, save_path, score_thr=0.5):
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    result = inference_detector(model, image_path)
+    pred_instances = result.pred_instances.cpu()
+
+    bboxes = pred_instances.bboxes.numpy()
+    labels = pred_instances.labels.numpy()
+    scores = pred_instances.scores.numpy()
+    masks = pred_instances.masks.numpy() if hasattr(pred_instances, 'masks') else None
+
+    class_names = model.dataset_meta['classes']
+
+    fig, ax = plt.subplots(1, figsize=(12, 8))
+    ax.imshow(image)
+    ax.axis('off')
+
+    height, width = image.shape[:2]
+
+    for i in range(len(bboxes)):
+        if scores[i] < score_thr:
+            continue
+
+        x1, y1, x2, y2 = bboxes[i]
+        label = labels[i]
+        caption = f'{class_names[label]}: {scores[i]:.2f}'
+
+        ax.add_patch(Rectangle((x1, y1), x2 - x1, y2 - y1,
+                               edgecolor='lime', facecolor='none', linewidth=2))
+        ax.text(x1, y1 - 2, caption, color='white', fontsize=10,
+                bbox=dict(facecolor='green', alpha=0.5, pad=1))
+
+        # assert masks is not None
+        # print(masks)
+        # if masks is not None:
+        #     mask = masks[i].astype(bool)
+        #     color = np.random.rand(3)
+        #     mask_image = np.zeros((height, width, 4))  # RGBA
+        #     for c in range(3):
+        #         mask_image[:, :, c] = color[c]
+        #     mask_image[:, :, 3] = mask.astype(float)
+
+        #     ax.imshow(mask_image)
+
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+    plt.tight_layout()
+    plt.savefig(save_path, bbox_inches='tight', pad_inches=0.1)
+    plt.close()
+
+
+import torch
+from mmengine.config import Config
+from mmdet.registry import MODELS
+from mmdet.utils import register_all_modules
+
+def load_model(config_path, checkpoint_path, device='cuda:0'):
+    register_all_modules()
+
+    cfg = Config.fromfile(config_path)
+    if 'pretrained' in cfg.model:
+        del cfg.model.pretrained
+
+    model = MODELS.build(cfg.model)
+    model.to(device)
+    model.eval()
+
+    checkpoint = torch.load(checkpoint_path, map_location='cpu', weights_only=False)
+    if 'state_dict' in checkpoint:
+        model.load_state_dict(checkpoint['state_dict'])
+    else:
+        model.load_state_dict(checkpoint)
+    if 'metainfo' in checkpoint:
+        model.dataset_meta = checkpoint['metainfo']
+    else:
+        model.dataset_meta = {
+            'classes': (
+                'aeroplane', 'bicycle', 'bird', 'boat', 'bottle',
+                'bus', 'car', 'cat', 'chair', 'cow', 'diningtable',
+                'dog', 'horse', 'motorbike', 'person', 'pottedplant',
+                'sheep', 'sofa', 'train', 'tvmonitor'
+            )
+        }
+    model.cfg = cfg
+    return model
+
+
+
+# 加载模型
+# mask_model = load_model('work_dirs/sparse_rcnn_voc/20250527_082051/vis_data/config.py', 'work_dirs/sparse_rcnn_voc/epoch_12.pth')
+mask_model = load_model('work_dirs/sparse_final/sparse_final.py', 'work_dirs/sparse_final/epoch_24.pth')
+# sparse_model = load_model('configs/voc_custom/sparse-rcnn_r50_fpn_1x_voc.py', 'work_dirs/sparse_rcnn/latest.pth')
+
+# 随机选取测试图像
+voc_test_images = [
+    'data/VOCdevkit/VOC2007/JPEGImages/000001.jpg',
+    'data/VOCdevkit/VOC2007/JPEGImages/000004.jpg',
+    'data/VOCdevkit/VOC2007/JPEGImages/000007.jpg',
+    'data/VOCdevkit/VOC2007/JPEGImages/000010.jpg',
+]
+
+# 可视化并保存
+for img_path in voc_test_images:
+    # visualize_prediction_with_masks(mask_model, img_path, f'../outputs/sparseinst/{os.path.basename(img_path)}')
+    visualize_prediction_with_masks(mask_model, img_path, f'../outputs/sparseinst_new/{os.path.basename(img_path)}')
